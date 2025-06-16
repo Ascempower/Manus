76e824ee000de4def3aead571e357f540543cdcc
Alicia Campbell <acampbellchoice@gmail.com>
2025-06-15 22:57:31 -0500

    deleted:    choice-insurance-hub-complete/manus/.eslintrc.json.backup

--- choice-insurance-hub-complete/manus/src/components/compliance/index.ts
+++
@@ -1,2 +0,0 @@
-export { default as HIPAANotice } from './HIPAANotice';
-
--- choice-insurance-hub-complete/manus/src/components/performance/index.ts
+++
@@ -1,3 +0,0 @@
-export { default as LazyLoad } from './LazyLoad';
-export { default as LazyImage } from './LazyImage';
-export { default as ResourceHints } from './ResourceHints';
