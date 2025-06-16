c8f70d32ea14704985feaf70820db9f996f44a35
Alicia Campbell <acampbellchoice@gmail.com>
2025-06-16 01:44:49 -0500

    modified:   src/app/blog/page.tsx

--- src/app/blog/page.tsx
+++ src/app/blog/page.tsx
@@ -2,7 +2,8 @@ import React from 'react';
 import { Metadata } from 'next';
 import Link from 'next/link';
 import Image from 'next/image';
-import { getAllBlogPosts, formatDate, generateExcerpt } from '@/lib/blog';
+import { getAllBlogPosts } from '@/lib/blog-server';
+import { formatDate, generateExcerpt } from '@/lib/blog-utils';
 
 export const metadata: Metadata = {
   title: 'Insurance Blog | Choice Insurance Hub - Expert Insurance Insights',
