d20d2796c4e701d5f601e8b2f27a0e33be8a0bc8
Alicia Campbell <acampbellchoice@gmail.com>
2025-06-20 20:34:30 -0500

    modified:   package.json

--- src/components/layout/ClientLayoutContent.tsx
+++ src/components/layout/ClientLayoutContent.tsx
@@ -2,18 +2,8 @@
 
 import dynamic from 'next/dynamic';
 
-const Header = dynamic(() => import('./Header'), {
-  ssr: false,
-  loading: () => (
-    <div className="flex h-16 items-center justify-between bg-brand-deep-forest-green px-4">
-      <div className="h-8 w-32 animate-pulse rounded bg-brand-deep-forest-green/50" />
-      <div className="flex gap-2">
-        <div className="h-8 w-20 animate-pulse rounded bg-brand-deep-forest-green/50" />
-        <div className="h-8 w-24 animate-pulse rounded bg-brand-deep-forest-green/50" />
-      </div>
-    </div>
-  ),
-});
+// Import Header directly since it needs to be interactive
+import Header from './Header';
 
 const Footer = dynamic(() => import('./Footer'), {
   ssr: false,
