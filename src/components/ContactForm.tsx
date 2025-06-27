7c8c8f10b61d8fcb43d51b18cfabac4df013ea9c
Alicia Campbell <acampbellchoice@gmail.com>
2025-06-26 20:23:10 -0500

    modified:   src/components/ContactForm.tsx

--- src/components/ContactForm.tsx
+++ src/components/ContactForm.tsx
@@ -10,10 +10,9 @@ export default function ContactForm() {
       <iframe 
         src="https://docs.google.com/forms/d/e/1FAIpQLSf69uAmSns6v8JsE_zedXjG0CTS-kNV5vaDrz9I57Bfa6i51A/viewform?embedded=true" 
         width="100%" 
-        height="1200" 
-        frameBorder="0" 
-        marginHeight="0"
-        marginWidth="0"
+        height={1200} 
+        frameBorder={0} 
+        style={{ marginHeight: 0, marginWidth: 0 }}
         title="Secure Contact Form"
         sandbox="allow-scripts allow-forms allow-same-origin"
         loading="lazy"
