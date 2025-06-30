340e60adba06bee86b3780c62b9eeba3ea895e15
Alicia Campbell <acampbellchoice@gmail.com>
2025-06-29 22:53:06 -0500

    modified:   public/contact-form.html

--- src/components/ContactForm.jsx
+++ src/components/ContactForm.jsx
@@ -4,16 +4,12 @@ export default function ContactForm() {
       dangerouslySetInnerHTML={{
         __html: `
           <div style="width: 100%; min-height: 1400px; padding: 2rem 0;">
-            <iframe
-              src="https://docs.google.com/forms/d/e/1FAIpQLScw-dgH0FXYkm_O-BjGXNVQrmXM9cwHdluCZ-D87oqE5aiPpg/viewform?embedded=true"
-              width="100%"
-              height="1400"
-              frameborder="0"
-              marginheight="0"
-              marginwidth="0"
-              style="border: none;">
-              Loading…
-            </iframe>
+            <div id="ff-compose"></div>
+            <script
+              async
+              defer
+              src="https://formfacade.com/include/108178716957596495252/form/1FAIpQLScw-dgH0FXYkm_O-BjGXNVQrmXM9cwHdluCZ-D87oqE5aiPpg/classic.js?div=ff-compose">
+            </script>
           </div>
         `,
       }}
